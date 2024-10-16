4ddda8e6bd77f76f0d3f83b1d90dbb0dbbb62218
sirchimex <chimex01.info@gmail.com>
2024-10-16 12:29:01 +0100

    working on the static files

--- lib_manager/settings.py
+++ lib_manager/settings.py
@@ -1,21 +1,25 @@
 from pathlib import Path
+from dotenv import load_dotenv
+
+import dj_database_url
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(os.path.join(BASE_DIR/".eVar", ".env"))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%+l_kogx$4e6l@sb9-#sw)20*%oa47jcikjo()4m-u&$0jjtmm'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get("DEBUG")
 
-ALLOWED_HOSTS = ['localhost','sirchimex.pythonanywhere.com']
-
+ALLOWED_HOSTS = ["127.0.0.1","localhost"]
+ALLOWED_HOSTS += os.environ.get("ALLOWED_HOSTS").split()
 
 # Application definition
 
@@ -65,11 +69,16 @@ WSGI_APPLICATION = 'lib_manager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default="sqlite://" + os.path.join(BASE_DIR, "db.sqlite3")
+    )
 }
 
 
